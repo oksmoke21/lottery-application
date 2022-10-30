7b442b368cd06356515d3e8db7bcf6f35fb06309
Ayush Ranjan <ayush.ranjan@solulab.com>
2022-10-31 03:30:10 +0530

    moved utils to client src

--- utils/contractConfig.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const fs = require("fs");
-
-const lotteryContractPath = path.join(__dirname, '../', "lotteryContract.json");
-const lotteryContract = JSON.parse(fs.readFileSync(lotteryContractPath));
-const contractAddress = lotteryContract.address;
-const contractAbi = lotteryContract.abi;
-
-export default { contractAddress, contractAbi };
