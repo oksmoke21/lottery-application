4ee807a6b21deea59a52f8a90149eb6c831d274a
Ayush Ranjan <ayush.ranjan@solulab.com>
2022-10-31 20:57:55 +0530

    restricted CORS to get methods

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use((req, res, next) => { // set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
